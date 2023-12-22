5ca101e0bb9b842f49b2d52faa4bf562869a3a3b
Surovtseva Daria <surovtseva.daria@gmail.com>
2023-12-23 01:24:59 +0300

    second commit

--- task4.sh
+++ task4.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
+
 help=false
 notchanges=false
 withchanges=false
-p=0
+withoptions=false
+
 while getopts ":hdv" option; do
-    p=1
+    withoptions=true
     if [ $option != "--" ]; then
-    case $option in
-        h) 
-            heip=true
+      case $option in
+        h)
+            help=true
             ;;
         d)
             notchanges=true
@@ -24,48 +26,66 @@ while getopts ":hdv" option; do
             echo "Wrong option."
             exit 1
             ;;
-        esac
+      esac
     fi
 done
-shift"$((OPTIND -1))"
-if $heip; then
-   echo "Usage: $0 [-h] [-d] [-v] <suffix> <file>>."
+
+# удаляем опции из агрументов
+if $withoptions; then
+    shift "$((OPTIND -1))"
+fi
+
+if $help; then
+   echo "Usage: $0 [-h] [-d] [-v] <suffix> -- <files>."
    exit 1
 fi
+
+# проверка наличия суффикса первым аргументом
 if [ -z "$1" ]; then
     echo "Enter suffix."
     exit 1
 fi
 
-if [ $p -eq 1 ]; then
+suffix=$1
+# удаляем суффикс как первый аргумент
+shift
+
+# удаляем "--" как первый аргумент, при налчиии
+if [ "$1" == "--" ]; then
     shift
 fi
 
-if [ "$1"=="--" ]; then
-    shift
-fi
+# все что осталось в аргументах - файлы. если их нет - ошибка
 
-if [ $# -eq 1 ]; then
+if [ $# -eq 0 ]; then
     echo "Enter files."
     exit 1
 fi
-suffix=$1
-shift
+
 for file in "$@"; do
-    if [ ! -e $file ]; then
-        echo "This file "$file" does not exist."
-	continue
+    if [ ! -e "$file" ]; then
+      echo "File \"$file\" does not exist."
+	    continue
     fi
-    if [ ! -w $file ]; then
-	echo "The file cannot be renamed"
-	continue
+    if [ ! -w "$file" ]; then
+	    echo "The file cannot be renamed"
+	    continue
     fi
-    new_name="${file%.*}${suffix}.${file##*.}"
-    if $notchanges; then
+    # когда нет расширения файла
+    if [ "${file}" == "${file##*.}" ]; then
+      new_name="${file%.*}${suffix}"
+    else
+      new_name="${file%.*}${suffix}.${file##*.}"
+    fi
+    if $notchanges;
+      then
         echo "Original name file: $file, new name file: $new_name"
+      else
+        if $withchanges; then
+          echo "New file name: $new_name"
+        fi
+        mv "$file" "$new_name"
     fi
-    if $withchanges; then
-        echo "New file name: $new_name"
-    fi
-    mv "$file" "$new_name"
 done
+
+exit $?
